d3d5473f9f164f6db67694168d117e083200c96a
tschernysh <tschernysh@gmail.com>
2021-12-04 01:02:54 +0200

    update for exon

---
+++ js/main.js
@@ -0,0 +1,48 @@
+let text = document.getElementsByClassName('content')[0]
+let preloaderBlock = document.getElementsByClassName('modal__block')[0]
+
+let currText = text.innerHTML.split('')
+let cyphrSymbols = '!#^&*-=/|+<>?qwertyuiop[]asdfghjkl;zxcvbnm,./'
+console.log(cyphrSymbols.split(''));
+text.innerHTML = null
+currText.map( (el, index) => text.appendChild(document.createElement('span')).id = index)
+
+const textCyphr = (currText, cyphrSymbols) => {
+    let letterPosition = currText.map(() => false)
+    console.log(letterPosition);
+    let isEnded = false
+    for(let i = 0; i < currText.length; i++){
+        let interval
+        let chance = 1
+        interval = setInterval(() => {
+            if(Math.random()*chance > Math.random()*200 ){
+                text.children[i].innerHTML = currText[i]    
+                text.children[i].className = 'right__letter'
+                letterPosition[i] = true
+                if(letterPosition.indexOf(false) === -1) {
+                    isEnded = true
+                    setTimeout(() => {
+                        text.innerHTML = 'exon center'
+                        text.classList.add('content__away')
+                        setTimeout(() => {
+                            preloaderBlock.classList.add('fade__block')
+                            setTimeout(() => {
+                                preloaderBlock.style.display = 'none'
+                            }, 1000);
+                        }, 1000)
+                    }, 1000)
+                }
+                clearInterval(interval)
+            }else{
+                text.children[i].innerHTML = cyphrSymbols[Math.floor((Math.random()*cyphrSymbols.length ))]
+                text.children[i].className = 'wrong__letter'
+                chance++
+            }
+        }, Math.random()*400)   
+    }
+}
+
+
+setTimeout(() => {
+    textCyphr(currText, cyphrSymbols)
+}, 1000)
